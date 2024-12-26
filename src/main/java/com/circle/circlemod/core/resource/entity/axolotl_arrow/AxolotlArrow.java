788c1c06b0d77e21019fa6b60090f2473042001a
yuanxin518 <hyx_hubei@qq.com>
2024-12-27 04:37:26 +0800

    feat: 美西螈箭

--- src/main/java/com/circle/circlemod/core/resource/entity/axolotl_arrow/AxolotlArrow.java
+++ src/main/java/com/circle/circlemod/core/resource/entity/axolotl_arrow/AxolotlArrow.java
@@ -2,19 +2,31 @@ package com.circle.circlemod.core.resource.entity.axolotl_arrow;
 
 import com.circle.circlemod.core.builds.register.CircleUniRegister;
 import com.circle.circlemod.core.resource.CircleResourceLocation;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.ParticleUtils;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageSources;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
 
 import javax.annotation.Nullable;
 
 
 public class AxolotlArrow extends AbstractArrow {
-    public AxolotlArrow(Level level, LivingEntity owner, ItemStack pickupItemStack, @Nullable ItemStack firedFromWeapon) {
+    public boolean isSafe = false;
+
+    public AxolotlArrow(Level level, LivingEntity owner, ItemStack pickupItemStack, @Nullable ItemStack firedFromWeapon, boolean isSafe) {
         super((EntityType<? extends AbstractArrow>) CircleUniRegister.ENTITIES_REGISTRY.get(CircleResourceLocation.AXOLOTL_ARROW)
                                                                                        .get(), owner, level, pickupItemStack, firedFromWeapon);
+        this.isSafe = isSafe;
     }
 
     public AxolotlArrow(EntityType<?> entityType, Level level) {
@@ -25,5 +37,29 @@ public class AxolotlArrow extends AbstractArrow {
     @Override
     protected ItemStack getDefaultPickupItem() {
         return null;
+    }
+
+    @Override
+    protected void onHitEntity(EntityHitResult result) {
+        Entity entity = result.getEntity();
+        float health = 0f;
+        if (entity instanceof LivingEntity) {
+            health = ((LivingEntity) entity).getHealth();
+        }
+
+        if (isSafe && entity instanceof LivingEntity) {
+            ((LivingEntity) entity).setHealth(health + 1);
+            entity.hurt(level().damageSources()
+                               .arrow(this, null), 0);
+
+            if (!level().isClientSide) {
+                ClientLevel level = Minecraft.getInstance().level;
+                if (level != null) {
+                    ParticleUtils.spawnParticles(level, entity.blockPosition(), 6, 1, 1, true, ParticleTypes.HEART);
+                }
+            }
+        } else {
+            super.onHitEntity(result);
+        }
     }
 }
--- src/main/java/com/circle/circlemod/core/resource/item/axolotl_bow/AxolotlBow.java
+++ src/main/java/com/circle/circlemod/core/resource/item/axolotl_bow/AxolotlBow.java
@@ -1,13 +1,20 @@
 package com.circle.circlemod.core.resource.item.axolotl_bow;
 
+import com.circle.circlemod.core.cability.AxolotlCability;
 import com.circle.circlemod.core.resource.entity.axolotl_arrow.AxolotlArrow;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 public class AxolotlBow extends BowItem {
+    public static AxolotlCability cability = new AxolotlCability();
+
     public AxolotlBow() {
         super(new Item.Properties().durability(2));
     }
@@ -18,8 +25,26 @@ public class AxolotlBow extends BowItem {
     }
 
     @Override
+    public boolean useOnRelease(ItemStack stack) {
+        cability.refreshRepairTimer();
+        return super.useOnRelease(stack);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+        cability.repire(level, stack);
+        super.inventoryTick(stack, level, entity, slotId, isSelected);
+    }
+
+    @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, @Nullable T entity, Consumer<Item> onBroken) {
+        if (stack.getMaxDamage() - stack.getDamageValue() > 1) return 1;
+        return 0;
+    }
+
+    @Override
     protected Projectile createProjectile(Level level, LivingEntity shooter, ItemStack weapon, ItemStack ammo, boolean isCrit) {
-        AbstractArrow abstractarrow =new AxolotlArrow(level, shooter, ammo.copyWithCount(1), weapon);
+        AbstractArrow abstractarrow = new AxolotlArrow(level, shooter, ammo.copyWithCount(1), weapon, this.getMaxDamage(weapon) - this.getDamage(weapon) <= 1);
         if (isCrit) {
             abstractarrow.setCritArrow(true);
         }
